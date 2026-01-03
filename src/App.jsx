36674fd90a142be05d58be743a37835bebe47a7f
alisaneuber <alisa.m.neuber@outlook.de>
2026-01-03 16:52:45 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
